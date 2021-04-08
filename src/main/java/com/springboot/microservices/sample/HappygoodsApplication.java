e84e27979a8650859c9c27eb7e8344722fead972
happyns <happy@cloud0>
2021-04-08 15:10:09 +0900

    change appname

--- src/main/java/com/springboot/microservices/sample/HappygoodsApplication.java
+++ src/main/java/com/springboot/microservices/sample/HappygoodsApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MvpSampleSpringbootApplication {
+public class HappygoodsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvpSampleSpringbootApplication.class, args);
+		SpringApplication.run(HappygoodsApplication.class, args);
 	}
 
 }
